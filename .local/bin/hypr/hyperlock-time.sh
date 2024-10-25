1fefa1edaa75a64b3a2982c40126cf0bb2bfd102
Andr3as07 <andr3as06@gmail.com>
2024-10-25 19:05:14 +0200

    Use the full name of the user

--- .local/bin/hypr/hyperlock-time.sh
+++ .local/bin/hypr/hyperlock-time.sh
@@ -4,13 +4,13 @@
 
 
 current_hour=$(date +"%H")
-user_string="$USER"
+user_string=$(cat /etc/passwd | grep $USER: | cut -d ':' -f 5)
 
-if [ "$current_hour" -ge 5 ] && [ "$current_hour" -lt 12 ]; then
+if [ "$current_hour" -ge 5 ] && [ "$current_hour" -lt 10 ]; then
     echo "Good morning, $user_string"
-elif [ "$current_hour" -ge 12 ] && [ "$current_hour" -lt 18 ]; then
+elif [ "$current_hour" -ge 10 ] && [ "$current_hour" -lt 17 ]; then
     echo "Good day, $user_string"
-elif [ "$current_hour" -ge 18 ] && [ "$current_hour" -lt 22 ]; then
+elif [ "$current_hour" -ge 17 ] && [ "$current_hour" -lt 22 ]; then
     echo "Good evening, $user_string"
 else
     echo "Good night, $user_string"
