48c3d08f38c8308bd4dc0eaeea460d31c7b5c7c7
Andr3as07 <andr3as06@gmail.com>
2024-12-04 21:38:21 +0100

    chore: add new Obsidian plugin configuration for markdown support
    - Added new Obsidian plugin configuration file.
    - Set plugin to load lazily with markdown file type.
    - Disabled frontmatter parsing in Obsidian settings.

---
+++ .config/nvim/lua/plugins/lang-obsidian-md.lua
@@ -0,0 +1,18 @@
+return {
+    "epwalsh/obsidian.nvim",
+    version = "*",  -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    opts = {
+        workspaces = {
+            {
+                name = "vault",
+                path = "~/Obsidian",
+            }
+        },
+        disable_frontmatter = true
+    }
+}
