ff600d0b3ef567f9d17436211637449bc714e592
Andr3as07 <andr3as06@gmail.com>
2024-12-04 21:37:36 +0100

    refactored `ui-lualine.lua` for improved code readability
    - Indentation fixed for consistent code formatting
    - Code comments retained for potential future use
    - Opts and config sections aligned for clarity
    - No functional changes made to configuration logic
    - Enhanced readability by adjusting indentation levels

--- .config/nvim/lua/plugins/ui-lualine.lua
+++ .config/nvim/lua/plugins/ui-lualine.lua
@@ -3,22 +3,22 @@ return {
     lazy = false,
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
-       sections = {
-           lualine_a = { 'mode' },
-           lualine_b = { 'branch', 'diff', 'diagnostics' },
-           lualine_c = { 'filename' },
-           lualine_x = { 'encoding', 'fileformat', 'filetype' },
-           lualine_y = { 'searchcount', 'selectioncount' },
-           lualine_z = { 'location', 'progress' }
-       }
+        sections = {
+            lualine_a = { 'mode' },
+            lualine_b = { 'branch', 'diff', 'diagnostics' },
+            lualine_c = { 'filename' },
+            lualine_x = { 'encoding', 'fileformat', 'filetype' },
+            lualine_y = { 'searchcount', 'selectioncount' },
+            lualine_z = { 'location', 'progress' }
+        }
     },
-  config = function(opts, _)
-	local lualine = require('lualine')
-	lualine.setup(opts)
-	  --      -- vim.opt.laststatus = 0
-      vim.opt.showmode = false
-      vim.opt.ruler = false
-      vim.opt.showcmd = false
-      vim.opt.statusline = ''
-  end
+    config = function(opts, _)
+        local lualine = require('lualine')
+        lualine.setup(opts)
+        -- vim.opt.laststatus = 0
+        vim.opt.showmode = false
+        vim.opt.ruler = false
+        vim.opt.showcmd = false
+        vim.opt.statusline = ''
+    end
 }
