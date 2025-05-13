4ae59899b1dbdb6cec8b376064c16ca6ce5018e5
Andr3as07 <andr3as06@gmail.com>
2025-05-13 22:57:52 +0200

    Add reainbow brackets

--- .config/nvim/lua/plugins/ui-rainbow.lua
+++ .config/nvim/lua/plugins/ui-rainbow.lua
@@ -1,7 +1,37 @@
 return {
-    "luochen1990/rainbow",
-    lazy = false,
-    config = function(opts)
-        vim.g.rainbow_active = 1
-    end
+    {
+        "luochen1990/rainbow",
+        lazy = false,
+        config = function(opts)
+            vim.g.rainbow_active = 1
+        end
+    },
+    {
+        "hiphish/rainbow-delimiters.nvim",
+        config = function()
+            vim.g.rainbow_delimiters = {
+                strategy = {
+                    [''] = 'rainbow-delimiters.strategy.global',
+                    vim = 'rainbow-delimiters.strategy.local',
+                },
+                query = {
+                    [''] = 'rainbow-delimiters',
+                    lua = 'rainbow-blocks',
+                },
+                priority = {
+                    [''] = 110,
+                    lua = 210,
+                },
+                highlight = {
+                    'RainbowDelimiterRed',
+                    'RainbowDelimiterYellow',
+                    'RainbowDelimiterBlue',
+                    'RainbowDelimiterOrange',
+                    'RainbowDelimiterGreen',
+                    'RainbowDelimiterViolet',
+                    'RainbowDelimiterCyan',
+                },
+            }
+        end
+    }
 }
