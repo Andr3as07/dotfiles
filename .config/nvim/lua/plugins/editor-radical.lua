679aed007915ee42879d5500a66e6be91cee5473
Andr3as07 <andr3as06@gmail.com>
2025-07-23 21:51:13 +0200

    Add radical and toggler

---
+++ .config/nvim/lua/plugins/editor-radical.lua
@@ -0,0 +1,7 @@
+return {
+    'glts/vim-radical',
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        'glts/vim-magnum'
+    }
+}
---
+++ .config/nvim/lua/plugins/editor-toggler.lua
@@ -0,0 +1,43 @@
+return {
+    'nguyenvukhang/nvim-toggler',
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+        inverses = {
+            ['vim'] = 'emacs',
+            ['true'] = 'false',
+            ['True'] = 'False',
+            ['yes'] = 'no',
+            ['on'] = 'off',
+            ['left'] = 'right',
+            ['up'] = 'down',
+            ['enable'] = 'disable',
+            ['!='] = '==',
+            ['1'] = '0',
+            ['0'] = '1',
+            ['break'] = 'continue',
+            ['continue'] = 'break',
+            ['get'] = 'set',
+            ['Get'] = 'Set',
+            ['set'] = 'get',
+            ['Set'] = 'Get',
+            ['struct'] = 'class',
+            ['class'] = 'enum',
+            ['enum'] = 'union',
+            ['union'] = 'struct',
+            ['||'] = '&&',
+            ['&&'] = '||',
+            ['|'] = '&',
+            ['&'] = '|',
+            ['++'] = '--',
+            ['--'] = '++',
+            ['->'] = '.',
+            ['.'] = '->',
+        },
+        remove_default_keybinds = false, -- default: <leader>i
+        remove_default_inverses = true,
+        autoselect_longest_match = true
+    },
+    config = function (_, opts)
+        require('nvim-toggler').setup(opts)
+    end
+}
