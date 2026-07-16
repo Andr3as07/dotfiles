0d1c11cbd3f5ba6ac5bd7254117f66fb8baf19a6
Andreas Evans <andr3as06@gmail.com>
2026-07-16 20:05:19 +0200

    Pin treesitter to master

--- .config/nvim/lua/plugins/editor-treesitter-text-objects.lua
+++ .config/nvim/lua/plugins/editor-treesitter-text-objects.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter-textobjects",
+  branch = "master",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   events = { "BufReadPre", "BufNewFile" },
   config = function()
